7fe0232826bc55aee3a341778100953f7bbd5455
Komal-patra <komalfsds2022@gmail.com>
2024-01-14 22:46:48 +0530

    Updated code as getting error during release

--- setup.py
+++ setup.py
@@ -1,31 +1,15 @@
-from setuptools import find_packages,setup
+from setuptools import setup, find_packages
 from typing import List
-
-HYPEN_E_DOT='-e .'
-
-"""
-
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements"""
-
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.2"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "mongodatabaseautomation"
-AUTHOR_USER_NAME = "Komal-patra"
-AUTHOR_EMAIL = "komalfsds2022@gmail.com"
+PKG_NAME= "mongo_connect"
+AUTHOR_USER_NAME = "sunnysavita10"
+AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
 
 setup(
     name=PKG_NAME,
@@ -41,5 +25,7 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirements("./requirements_dev.txt"),
     )
+
+
+
